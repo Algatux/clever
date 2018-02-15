fd2c3dedb8c4cfcb5e88b7ec9d2591441fdbae91
Alessandro Galli <alessandro.galli@facile.it>
2018-02-15 09:49:01 +0100

    docker env for imap init

---
+++ src/Plugins/EmailFetch/EmailFetchProvider.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+namespace Clever\Plugins\EmailFetch;
+
+use Clever\Providers\CleverServiceProvider;
+
+class EmailFetchProvider extends CleverServiceProvider
+{
+
+    public function registerParameters()
+    {
+    }
+
+    public function registerServices()
+    {
+    }
+
+    public function registerConsoleCommands()
+    {
+    }
+}
